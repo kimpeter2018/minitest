43026c6040a6786f45602e9f3a18cc9e4b913b30
Yertle <kimpeter2018@naver.com>
2021-04-22 23:56:04 +0900

    make saveData and loadData Functions

--- manager.c
+++ manager.c
@@ -40,7 +40,9 @@ void saveData(Product p[], int count){
 
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
-	
+	for(int i =0; i<count;i++){
+	fprintf(fp, "%d %d %s\n", p[i].weight, p[i].price, p[i].name);
+	}	
 	
 	fclose(fp);
 	printf("저장됨!\n");
@@ -53,8 +55,15 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+	fp = fopen("product.txt", "r+");
+	for(; count<100;count++){
+		fscanf(fp, "%d", &p[count].weight);
+		if(feof(fp)) break;
+		fscanf(fp, "%d", &p[count].price);
+		fscanf(fp, "%s", p[count].name);	
 
-
+	}
+	fclose(fp);
 
 
 	printf("=> 로딩 성공!\n");
